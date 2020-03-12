d05c3182344707579b04b89658705a85d90e6bd3
Eric Avdey <eiri@eiri.ca>
2020-03-11 22:11:28 -0300

    Char encoder/decoder

--- encoding/base69/base69.go
+++ encoding/base69/base69.go
@@ -1,6 +1,19 @@
 package base69
 
-// Placeholder here just to make sure things are running
-func Placeholder() string {
-	return "ohai!"
+import (
+	"strings"
+)
+
+const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/-*<>|"
+
+func intToBytes(n int) []byte {
+	i := n % 69
+	j := n / 69
+	return []byte{charset[i], charset[j]}
+}
+
+func bytesToInt(b []byte) int {
+	i := strings.Index(charset, string(b[0]))
+	j := strings.Index(charset, string(b[1]))
+	return 69*j + i
 }
--- encoding/base69/base69_test.go
+++ encoding/base69/base69_test.go
@@ -1,13 +1,38 @@
 package base69
 
 import (
+	"reflect"
 	"testing"
 )
 
-// TestPlaceholder to make sure things are running
-func TestPlaceholder(t *testing.T) {
-	hello := Placeholder()
-	if hello != "ohai!" {
-		t.Error("Placeholder should say ohai!")
+var ibPairs = []struct {
+	i int
+	b []byte
+}{
+	{0, []byte("AA")},
+	{1, []byte("BA")},
+	{68, []byte("|A")},
+	{69, []byte("AB")},
+	{1890, []byte("bb")},
+	{3640, []byte("00")},
+	{4340, []byte("++")},
+	{4760, []byte("||")},
+}
+
+func TestIntToBytes(t *testing.T) {
+	for _, p := range ibPairs {
+		got := intToBytes(p.i)
+		if !reflect.DeepEqual(got, p.b) {
+			t.Errorf("intToBytes(%d) = %q, want %q", p.i, got, p.b)
+		}
+	}
+}
+
+func TestBytesToInt(t *testing.T) {
+	for _, p := range ibPairs {
+		got := bytesToInt(p.b)
+		if got != p.i {
+			t.Errorf("bytesToInt(%q) = %d, want %d", p.b, got, p.i)
+		}
 	}
 }
